d52ee3b0d3b1cc90f38d27e87412b50b812837bf
mandeep4u <mandeepsgh1@gmail.com>
2017-12-24 19:08:53 +0530

    eight try

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "Hello World, Ki hal hai, ab thik h, kab chalke dikhayega bhai, ab to chalja bhai, thik h ab khush h na"
+echo "Hello World, Ki hal hai, ab thik h, kab chalke dikhayega bhai"
 
 ?>
